8195a8c4daaf8c34de7069203ed6b92e2dcf6527
Kiyoshi Aman <kiyoshi.aman@gmail.com>
2018-10-12 17:04:04 -0500

    package.sh: More packaging fixups.

--- package.sh
+++ package.sh
@@ -2,7 +2,7 @@
 
 DIR="smeargle-$1"
 
-rm -f smeargle/__pycache__/* && rmdir smeargle/__pycache__
+rm -rf smeargle/__pycache__
 mkdir ${DIR}
 cp -r smeargle ${DIR}
 cp -a smeargle.py porygon.py girafarig.py readme.txt melissa8.{png,json} test.txt example.json ${DIR}
